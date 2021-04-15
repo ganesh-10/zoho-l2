8dee08ceeb3da842341229bce5986ffcd30384d2
ganesh-10 <76418379+ganesh-10@users.noreply.github.com>
2021-04-15 10:41:50 +0530

    Update nzohoq1.py

--- nzohoq1.py
+++ nzohoq1.py
@@ -1,11 +1,11 @@
 def pypart2(s):
 	n=len(s)
 	mid=int(len(s)/2)
-	k = 2*n - 2
+	k = n - 1
 	for i in range(0, n):
 		for j in range(0, k):
 			print(end=" ")
-		k = k - 2
+		k = k - 1
 		for j in range(0, i+1):
 		
 			if(j+mid)<n:
